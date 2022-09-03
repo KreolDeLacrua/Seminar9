4bf2ee78c97412f917869909f64595aef20ebd7f
KreolDeLacrua <kreol_11@inbox.com>
2022-09-03 17:37:21 +0300

    task67

---
+++ task67/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 67: Напишите программу, 
+// которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите N ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = PrintNumbersToN(number);
+Console.WriteLine(sum);
+
+int PrintNumbersToN(int n)
+{
+    int count = 0;
+    if (n <= 1) return 0;
+    count = n % 10;
+    n = n / 10;
+    return count + PrintNumbersToN(n);
+}
